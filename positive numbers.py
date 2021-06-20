32b515c43c42bc10cccb595a7d49e0fe320a485e
RowenaMenezes <84384811+RowenaMenezes@users.noreply.github.com>
2021-06-20 21:02:54 +0530

    Add files via upload

---
+++ positive numbers.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+nums= [12,17,5,64,-14]
+print("Original numbers in the list:",nums)
+new_nums = list(filter(lambda x:x>0,nums))
+print("Positive numbers in the list:",new_nums)
+
+
+# In[ ]:
+
+
+
+
